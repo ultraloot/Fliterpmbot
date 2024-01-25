677e5300f989bc7194ec9da5a546a4a4ef1e7265
KD Botz <116437604+KDBotz@users.noreply.github.com>
2024-01-25 15:43:21 +0530

    Update users_chats_db.py

--- database/users_chats_db.py
+++ database/users_chats_db.py
@@ -9,7 +9,6 @@ class Database:
         self.db = self._client[database_name]
         self.col = self.db.users
         self.grp = self.db.groups
-        self.req = self.db.requests
 
 
     def new_user(self, id, name):
@@ -32,12 +31,6 @@ class Database:
                 reason="",
             ),
         )
-
-    async def find_join_req(self, id):
-        return bool(await self.req.find_one({'id': id}))
-        
-    async def add_join_req(self, id):
-        await self.req.insert_one({'id': id})
  
     async def add_user(self, id, name):
         user = self.new_user(id, name)
