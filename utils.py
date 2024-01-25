f697a1695b6786bd3f6603fb494b45e75c1f31ea
KD Botz <116437604+KDBotz@users.noreply.github.com>
2024-01-25 21:56:15 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -3,6 +3,7 @@ from pyrogram.errors import InputUserDeactivated, UserNotParticipant, FloodWait,
 from info import AUTH_CHANNEL, LONG_IMDB_DESCRIPTION, MAX_LIST_ELM, SHORTLINK_URL, SHORTLINK_API, IS_SHORTLINK, LOG_CHANNEL, TUTORIAL, GRP_LNK, CHNL_LNK, CUSTOM_FILE_CAPTION, REQ_CHANNEL, ADMINS, SECOND_SHORTLINK_URL, SECOND_SHORTLINK_API
 from imdb import Cinemagoer 
 import asyncio
+from database.join_reqs import JoinReqs as db2
 from pyrogram.types import Message, InlineKeyboardButton, InlineKeyboardMarkup
 from pyrogram.errors import FloodWait, UserIsBlocked, MessageNotModified, PeerIdInvalid
 from pyrogram import enums
