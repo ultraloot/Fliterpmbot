7562570f0c44adb802d5399fc5433fba9d21dc0e
KD Botz <116437604+KDBotz@users.noreply.github.com>
2024-01-26 09:04:10 +0530

    Update fsub.py

--- plugins/fsub.py
+++ plugins/fsub.py
@@ -95,7 +95,7 @@ async def ForceSub(bot: Client, update: Message, file_id: str = False, mode="che
             return True
     except UserNotParticipant:
         text="""**Fɪʀsᴛ ᴄʟɪᴄᴋ ᴏɴ Rᴇǫᴜᴇꜱᴛ Tᴏ Jᴏɪɴ Cʜᴀɴɴᴇʟ ʙᴜᴛᴛᴏɴ . ᴛʜᴇɴ ᴄᴏᴍᴇ ʙᴀᴄᴋ ᴛᴏ ᴛʜᴇ ʙᴏᴛ ᴄʟɪᴄᴋ ᴏɴ ᴍᴇ ᴊᴏɪɴᴇᴅ ʙᴜᴛᴛᴏɴ ᴛᴏ ɢᴇᴛ ᴛʜᴇ ғɪʟᴇ...**"""
-        kk, file_id = message.command[1].split("_", 1)
+        kk, file_id = update.command[1].split("_", 1)
         buttons = [
             [
                 InlineKeyboardButton("🎗 Rᴇǫᴜᴇꜱᴛ Tᴏ Jᴏɪɴ Cʜᴀɴɴᴇʟ 🎗", url=invite_link)
